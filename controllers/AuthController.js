5a9562eee2163fe48cd12ede4fc58a3dbc4f53e6
Ashish Soni <4840asoni@gmail.com>
2023-08-12 17:11:48 +0530

    create signup login controller

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,9 @@
 const User = require("../models/UserModel");
 const OTP = require("../models/OTP");
 const otpGenerator = require("otp-generator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 
 // send otp
@@ -68,7 +71,7 @@ exports.signup = async (req, res) => {
         const { firstName, lastName, email, password, confirmPassword, accountType, contactNumber, otp } = req.body;
 
         // validate karlo
-        if (!firstName || !lastName || !email || !password || !confirmPassword || !contactNumber || !otp) {
+        if (!firstName || !lastName || !email || !password || !confirmPassword || !otp) {
             return res.status(403).json({
                 success: false,
                 message: "All fields are rquired!"
@@ -151,5 +154,71 @@ exports.signup = async (req, res) => {
 }
 
 // login
+exports.login = async (req, res) => {
+    try {
+        // get data from req body
+        const { email, password } = req.body;
+        // validation data
+        if (!email || !password) {
+            return res.status(403).json({
+                success: false,
+                message:"All fields are require",
+            })
+        }
+        // user exist or not
+        const user = await User.findOne({ email }).populate("additionalDetails");
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message:"User is not registered, Please signup first"
+            })
+        }
+        // generate JWT, after password matching
+        if (await bcrypt.compare(password, user.password)) {
+            const payload = {
+                email: user.email,
+                id: user._id,
+                accountType:user.accountType,
+            }
+            const token = jwt.sign(otpPayload, process.env.JWT_SECRET, {
+                expiresIn:"2h",
+            })
+            user.token = token;
+            user.password = undefined
+            
+            // create cookie  and send response
+            const options = {
+                expires: new Date(Date.now() + 3 * 34 * 60 * 60 * 1000),
+                httpOnly:true,
+            }
+            res.cookie("token", token, options).status(200).json({
+                success: true,
+                message:"Login Successfully!"
+            })
+        } else {
+            return res.status(401).json({
+                success: false,
+                message: "Password is incorrect"
+            });
+        }
+        
+        
+    } catch (error) {
+         console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Login failure, Please try again! ",
+        })
+    }
+}
 
 // forgot password
+exports.changePassword = async (req, res) => {
+    // get data from req body
+    // get oldPassword, newPassword, confirmPassword
+    // validation
+
+    // update password in db
+    // send mail - password update
+    // return response
+}
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,1 +1,93 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const User = require("../models/UserModel")
+// auth
+exports.auth = async (req, res) => {
+    try {
+        // check json web token
+        // extract token
+        const token = req.cookies.token || req.body || req.header("Authorisation").replace("Bearer ", "");
 
+        // if token is missing , then return response
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message:'Token is missing',
+            })
+        }
+
+        // verify the token
+        try {
+            const decode = await jwt.verify(token, process.env.JWT_SECRET);
+            console.log(decode);
+            res.user = decode;
+        } catch (error) {
+            // verification - issue
+            return res.status(401).json({
+                success: false,
+                message:'token is missing'
+            })
+        }
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message:"Something went wrong while validating the token",
+        })
+    }
+}
+
+
+// isStudent
+exports.isStudent = async (req, res,next) => {
+    try {
+        if (req.user.accountType !== "Student") {
+            return res.status(401).json({
+                success: false,
+                message:"This is a protected route for Students only"
+            })
+        }
+        next();
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message:"User role cannot be verified, try again",
+        })
+    }
+}
+
+// isInstructor
+exports.isInstructor = async (req, res,next) => {
+    try {
+        if (req.user.accountType !== "Instructor") {
+            return res.status(401).json({
+                success: false,
+                message:"This is a protected route for Instructor only"
+            })
+        }
+        next();
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message:"User role cannot be verified, try again",
+        })
+    }
+}
+
+// isAdmin
+exports.isAdmin = async (req, res,next) => {
+    try {
+        if (req.user.accountType !== "Admin") {
+            return res.status(401).json({
+                success: false,
+                message:"This is a protected route for Admin only"
+            })
+        }
+        next();
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message:"User role cannot be verified, try again",
+        })
+    }
+}
