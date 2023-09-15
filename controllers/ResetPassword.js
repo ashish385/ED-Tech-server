bc154d83555932e811ad2382cd220170958eb13a
Ashish Soni <4840asoni@gmail.com>
2023-09-15 11:45:11 +0530

    set mongoose version "mongoose": "^6.8.0"

--- controllers/ResetPassword.js
+++ controllers/ResetPassword.js
@@ -12,7 +12,7 @@ exports.resetPasswordToken = async (req, res) => {
 		if (!user) {
 			return res.json({
 				success: false,
-				message: `This Email: ${email} is not Registered With Us Enter a Valid Email `,
+				message: ` ${email} is not Registered With Us Enter a Valid Email `,
 			});
 		}
 		const token = crypto.randomBytes(20).toString("hex");
